3c7dd3abb5a84c91ce2b085b0f95da7c627af948
elinanomade <elinanomad@gmail.com>
2022-11-25 18:51:01 +0100

    const

--- GuessNumber/scriptnumber.js
+++ GuessNumber/scriptnumber.js
@@ -3,20 +3,18 @@
 
 
  // random value generated
- var y = Math.floor(Math.random() * 10 + 1);
+ const y = Math.floor(Math.random() * 10 + 1);
       
  // counting the number of guesses
  // made for correct Guess
- var guess = 1;
+ let guess = 1;
    
  document.getElementById("submitguess").onclick = function(){
    
 
 // number guessed by user     
-var x = document.getElementById("guessField").value;
-
-if(x == y)
-{    
+const x = document.getElementById("guessField").value;
+if(x == y){  
     alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
             + guess + " GUESS ");
 }
